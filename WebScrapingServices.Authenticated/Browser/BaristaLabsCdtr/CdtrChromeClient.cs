dcb946816ea731f7f7a3dd78cc59800f3a592824
Tomáš Krupka <hribayz@gmail.com>
2021-10-01 12:14:03 +0100

    Continue implementing CTDR client.

--- WebScrapingServices.Authenticated/Browser/BaristaLabsCdtr/CdtrChromeClient.cs
+++ WebScrapingServices.Authenticated/Browser/BaristaLabsCdtr/CdtrChromeClient.cs
@@ -49,16 +49,23 @@ namespace WebScrapingServices.Authenticated.Browser.BaristaLabsCdtr
         {
             // see https://github.com/BaristaLabs/chrome-dev-tools-runtime/blob/master/ChromeDevToolsCLI/Program.cs
 
-            // "user-data-dir=C:\SeleniumProfiles\" + config.UserName + "\\"
-
             var browserArgs = new StringBuilder()
                 .Append("--remote-debugging-port=").Append(settings.RemoteDebuggingPort)
                 .Append(@" --user-data-dir=C:\CdtrProfiles\").Append(settings.UserProfileName).Append('\\');
 
+            if (settings.UseProxy)
+            {
+                browserArgs.Append(" --proxy-server=").Append(settings.Proxy?.Address ?? throw new ApplicationException("Null proxy when UseProxy flag was up."));
+                if (browserArgs[^1] == '/')
+                {
+                    browserArgs.Remove(browserArgs.Length - 1, 1);
+                }
+            }
+
             Process.Start(settings.BrowserLocation, browserArgs.ToString());
 
             using var httpClient = new HttpClient();
-            var remoteSessions = await httpClient.GetStringAsync("http://localhost:9223/json");
+            var remoteSessions = await httpClient.GetStringAsync($"http://localhost:{settings.RemoteDebuggingPort}/json");
             var sessionInfos = JsonConvert.DeserializeObject<List<ChromeSessionInfo>>(remoteSessions);
 
             var chromeSession = new ChromeSession(sessionInfos.First(x => x.Type == "page").WebSocketDebuggerUrl);
@@ -154,6 +161,11 @@ namespace WebScrapingServices.Authenticated.Browser.BaristaLabsCdtr
 
                 var objectId = result.Result.ObjectId;
 
+                if (objectId == null)
+                {
+                    return null;
+                }
+
                 return new CdtrElement(objectId, _chromeSession, _jsBuilder);
 
             }
@@ -165,43 +177,78 @@ namespace WebScrapingServices.Authenticated.Browser.BaristaLabsCdtr
 
         }
 
-        public Task<IElement?> WaitUntilElementPresentAsync(string cssSelector, CancellationToken cancellationToken, PollSettings settings)
+        public async Task<IElement?> WaitUntilElementPresentAsync(string cssSelector, CancellationToken cancellationToken, PollSettings pollSettings)
         {
-            throw new NotImplementedException();
+            var stopwatch = Stopwatch.StartNew();
+
+            while (!cancellationToken.IsCancellationRequested && stopwatch.ElapsedMilliseconds < pollSettings.TimeoutMs)
+            {
+                var element = await FindElementByCssSelectorAsync(cssSelector);
+                if (element != null)
+                {
+                    return element;
+                }
+                else
+                {
+                    await Task.Delay(pollSettings.PeriodMs);
+                }
+            }
+
+            return null;
         }
     }
 
     public class CdtrElement : IElement
     {
+        private string _objectId;
+        private ChromeSession _chromeSession;
+
         public CdtrElement(string objectId, ChromeSession chromeSession, IJsBuilder jsBuilder)
         {
-            throw new NotImplementedException();
+            _objectId = objectId ?? throw new ArgumentNullException(nameof(objectId));
+            _chromeSession = chromeSession;
         }
-        public Task ClickAsync()
+        public async Task ClickAsync()
         {
-            throw new NotImplementedException();
-        }
-
-        public Task SendKeysAsync(string keys)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task SendKeysAsync(string keys, int delayEachMs)
-        {
-            throw new NotImplementedException();
+            await _chromeSession.Runtime.CallFunctionOn(new Runtime.CallFunctionOnCommand
+            {
+                ObjectId = _objectId,
+                FunctionDeclaration = "function() {this.click();}"
+            });
         }
 
-        public Task SendSpecialKeyAsync(SpecialKey key)
+        public async Task SendKeysAsync(string keys)
         {
-            throw new NotImplementedException();
+            await _chromeSession.Input.InsertText(new InsertTextCommand
+            {
+                Text = keys
+            });
+        }
+
+        public async Task SendKeysAsync(string keys, int delayEachMs)
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                var key = keys[i];
+                await _chromeSession.Input.DispatchKeyEvent(new DispatchKeyEventCommand
+                {
+                    Type = "char",
+                    Key = key.ToString()
+                });
+                await Task.Delay(delayEachMs);
+            }
+        }
+
+        public async Task SendSpecialKeyAsync(SpecialKey key)
+        {
+            await _chromeSession.Input.DispatchKeyEvent(key.ToDispatchKeyEventCommand());
         }
     }
 
     /// <summary>
-    /// Unlike the Selenium element, this is a description of the element when the DOM was last seen rather than a reference to it.
+    /// This is a description of the element when the DOM was last seen, not a reference to it.
     /// Every call the selector is evaluated against the current state of the DOM.
-    /// Two subsequent calls to the same <see cref="CdtrElementDescriptor"/> may therefore result in interactions with two different objects in the DOM.
+    /// Two subsequent calls to the same <see cref="CdtrElementDescriptor"/> instance may therefore result in interactions with two different objects in the DOM.
     /// </summary>
     public class CdtrElementDescriptor : IElement
     {
@@ -259,11 +306,13 @@ namespace WebScrapingServices.Authenticated.Browser.BaristaLabsCdtr
         {
             return specialKey switch
             {
-                SpecialKey.Backspace => new DispatchKeyEventCommand { Code = "Backspace" },
-                SpecialKey.Enter => new DispatchKeyEventCommand { Code = "Enter" },
-                SpecialKey.Escape => new DispatchKeyEventCommand { NativeVirtualKeyCode = 27, WindowsVirtualKeyCode = 27 },
-                SpecialKey.Tab => new DispatchKeyEventCommand { Code = "Tab" }
+                SpecialKey.Backspace => new DispatchKeyEventCommand { Code = "Backspace", Type = "char" },
+                SpecialKey.Enter => new DispatchKeyEventCommand { Code = "Enter", Type = "char" },
+                SpecialKey.Escape => new DispatchKeyEventCommand { NativeVirtualKeyCode = 27, WindowsVirtualKeyCode = 27, Type = "char" },
+                SpecialKey.Tab => new DispatchKeyEventCommand { Code = "Tab", Type = "char" },
+                _ => throw new NotImplementedException()
             };
+
         }
     }
 
