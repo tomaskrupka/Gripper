2709de47956eb753e453169398dd9c78d352fd03
Tomáš Krupka <tomas@tomaskrupka.eu>
2022-01-17 12:29:11 +0000

    Add Tests project.

---
+++ test/Gripper.Test.Common/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Gripper.Test.Common
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
