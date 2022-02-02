a444f50002f4c767464785c80dea2a13cbda44aa
Tomáš Krupka <tomas@tomaskrupka.eu>
2022-02-02 09:50:44 +0000

    move implementation to Gripper.WebClient project

--- src/Gripper.WebClient/Cdtr/Extensions/ServiceCollectionExtensions.cs
+++ src/Gripper.WebClient/Cdtr/Extensions/ServiceCollectionExtensions.cs
@@ -1,11 +1,6 @@
 ﻿using Gripper.WebClient.Cdtr;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Gripper.WebClient.Extensions
 {
